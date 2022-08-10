e18b1e61d76b2567f59c8150d08b818bb3f8518a
dickymujantara <dickymujantara@student.telkomuniversity.ac.id>
2022-08-10 13:45:18 +0700

    improve ui

--- src/OCR.js
+++ src/OCR.js
@@ -2,6 +2,8 @@ import React, {Component} from "react";
 import Webcam from "react-webcam";
 import Resizer from "react-image-file-resizer";
 import Tesseract from 'tesseract.js';
+import { Row, Col, Card, CardHeader, CardBody, CardFooter, Button } from "reactstrap";
+
 const FACING_MODE_USER = "user";
 const FACING_MODE_ENVIRONMENT = "environment";
 
@@ -11,7 +13,6 @@ export default class OCR extends Component{
         super(props)
         this.webcamRef = React.createRef()
         this.state = {
-            captureImage : null,
             resizeImage : null,
             resultText : null,
             faceMode : FACING_MODE_USER,
@@ -25,7 +26,6 @@ export default class OCR extends Component{
 
     capturePhoto = () => {
         const image = this.webcamRef.current.getScreenshot()
-        this.setState({captureImage : image})
         this.imageResizer(image)
         console.log(image);
     }
@@ -80,50 +80,92 @@ export default class OCR extends Component{
     }
 
     photoScan = () => {
-        const resultText = this.textRecog(this.state.resizeImage)
-        console.log("Result Text");
+        this.textRecog(this.state.resizeImage)
     }
 
     render(){
         return(
-            <div>
-                <div>
-                <Webcam
-                    ref={this.webcamRef}
-                    audio={false}
-                    height={720}
-                    screenshotFormat="image/jpeg"
-                    width={1280}
-                    videoConstraints={{
-                        facingMode: this.state.faceMode
-                    }}
-                />
-                <button onClick={() => this.switchCamera()}>Switch Camera</button>
-                <button onClick={() => this.capturePhoto()}>Capture Photo</button>
-                </div>
+            <Row>
+                <Col xs="12">
+                    <Row>
+                        <Col xs="1"></Col>
+                        <Col className="text-center" xs="10">
+                            <Card>
+                                <CardHeader>
+                                    <h2>Take Picture</h2>
+                                </CardHeader>
+                                <CardBody>
+                                    <Webcam
+                                        ref={this.webcamRef}
+                                        audio={false}
+                                        height={720}
+                                        screenshotFormat="image/jpeg"
+                                        width={1280}
+                                        videoConstraints={{
+                                            facingMode: this.state.faceMode
+                                        }}
+                                    />
+                                </CardBody>
+                                <CardFooter>
+                                    <Row>
+                                        <Col>
+                                            <Button className="mx-2" color="primary" onClick={() => this.switchCamera()}>Switch Camera</Button>
+                                            <Button color="primary" onClick={() => this.capturePhoto()}>Capture Photo</Button>
+                                        </Col>
+                                    </Row>
+                                </CardFooter>
+                            </Card>
+                        </Col>
+                        <Col xs="1"></Col>
+                    </Row>
+                </Col>
                 
-                <div>
-                    <h1>Before</h1>
-                    <img src={this.state.captureImage}/>
-                    <h1>After</h1>
-                    <img src={this.state.resizeImage}/>
-                    <button onClick={() => this.photoScan()}>Scan Photo</button>
-                </div>
-                
-                <div>
-                    <h1>Raw Text</h1>
-                    <span>{this.state.resultText}</span>
-                    <h1>Convert to Array:</h1>
-                    {
-                        this.state.arrText.map(e => {
-                            return(
-                                <p>{e}</p>
-                            )
-                        })
-                    }
-                </div>
-
-            </div>
+                <Col className="mt-3" xs="12">
+                    <Row>
+                        <Col xs="1"></Col>
+                        <Col className="text-center" xs="10">
+                            <Card>
+                                <CardHeader>
+                                    <h2>Result Image</h2>
+                                </CardHeader>
+                                <CardBody>
+                                    <Row>
+                                        <Col>
+                                            <img src={this.state.resizeImage}/>
+                                        </Col>
+                                    </Row>
+                                    {
+                                        this.state.arrText.length > 0 
+                                        ? <Row>
+                                            <Col>
+                                                <h6>Raw Text</h6>
+                                                <span>{this.state.resultText}</span>
+                                                <h6>Convert to Array:</h6>
+                                                {
+                                                    this.state.arrText.map(e => {
+                                                        return(
+                                                            <p>{e}</p>
+                                                        )
+                                                    })
+                                                }
+                                            </Col>
+                                        </Row>
+                                        : <></>
+                                    }
+                                </CardBody>
+                                <CardFooter>
+                                    <Row>
+                                        <Col>
+                                            <Button className="mx-2" color="primary" onClick={() => this.photoScan()}>Scan Photo</Button>
+                                        </Col>
+                                    </Row>
+                                </CardFooter>
+                            </Card>
+                        </Col>
+                        <Col xs="1"></Col>
+                    </Row>
+                </Col>
+            </Row>
         )
     }
 }
